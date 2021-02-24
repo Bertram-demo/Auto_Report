a61b9c9f58d9bfee2a1537f1174cf1efd750a16d
BertraMoon <bertramoon@126.com>
2021-02-24 22:37:23 +0800

    test6

--- test.py
+++ test.py
@@ -1,11 +1,15 @@
 from selenium import webdriver
 import time
 import sys
+from pyvirtualdisplay import Display
+
 
 try:
     username = sys.argv[1]
     password = sys.argv[2]
 
+    display = Display(visible=0, size=(800, 800))  
+    display.start()
     browser = webdriver.Chrome()
     browser.get('https://yqfk.dgut.edu.cn')
     User = browser.find_element_by_id('username')
@@ -14,8 +18,8 @@ try:
     Pwd.send_keys(password)
     button = browser.find_element_by_id('loginBtn')
     button.click()
-    browser.get('https://yqfk.dgut.edu.cn')
-    print(button.text)
+    # browser.get('https://yqfk.dgut.edu.cn')
+    # print(button.text)
     print(browser.page_source)
     
     browser.close()
