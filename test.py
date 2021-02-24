b0d0e93a49ede9e53d7c7da0857a780797b72d64
BertraMoon <bertramoon@126.com>
2021-02-25 00:57:52 +0800

    test16

--- test.py
+++ test.py
@@ -14,20 +14,27 @@ try:
     display = Display(visible=0, size=(800, 800))  
     display.start()
     browser = webdriver.Chrome()
-    browser.get('https://cas.dgut.edu.cn/home/Oauth/getToken/appid/illnessProtectionHome/state/home.html')
+    # browser.get('https://cas.dgut.edu.cn/home/Oauth/getToken/appid/illnessProtectionHome/state/home.html')
+    browser.get('https://yqfk.dgut.edu.cn')
     User = browser.find_element_by_id('username')
     User.send_keys(username)
     Pwd = browser.find_element_by_id('casPassword')
     Pwd.send_keys(password)
     button = browser.find_element_by_id('loginBtn')
     button.click()
-    browser.get('https://yqfk.dgut.edu.cn')
-    wait = WebDriverWait(browser, 600)
-    result = wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'remind___fRE9P')))
-    button = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '.am-button-primary')))
-    print(result.text)
+    # browser.implicitly_wait(20)
+    wait = WebDriverWait(browser, 60)
+    result =  wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, 'remind___fRE9P')))
     # print(browser.page_source)
-    if "尚未打卡" in result.text:
+    if result[0].text == "":
+        time.sleep(1)
+        result =  wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, 'remind___fRE9P')))
+    button = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '.am-button-primary')))
+    for i in result:
+        print(i.text)
+    a = [i.text for i in result]
+    # print(a)
+    if "您今日尚未打卡" in a:
         button.click()
         print("已提交")
     else:
